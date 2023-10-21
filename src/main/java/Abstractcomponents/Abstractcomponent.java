66d399c6ac92fbf4dcc84210a2809671cf95ae83
Sharath-1999 <sharathchandraracha@gmail.com>
2023-10-21 22:26:54 +0530

    this commit is from Gitrepo

--- src/main/java/Abstractcomponents/Abstractcomponent.java
+++ src/main/java/Abstractcomponents/Abstractcomponent.java
@@ -44,6 +44,11 @@ public class Abstractcomponent  {
 		Thread.sleep(1000);
 //		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 //		wait.until(ExpectedConditions.invisibilityOf(ele));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
 	}
 	
 	public CartPage goToCartPage()
