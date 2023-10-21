debc001abf6703efcbc0d5011b5937756beef010
Sharath-1999 <sharathchandraracha@gmail.com>
2023-10-21 22:39:05 +0530

    testrepoyy testing commit

--- src/main/java/Abstractcomponents/Abstractcomponent.java
+++ src/main/java/Abstractcomponents/Abstractcomponent.java
@@ -44,7 +44,7 @@ public class Abstractcomponent  {
 		Thread.sleep(1000);
 //		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 //		wait.until(ExpectedConditions.invisibilityOf(ele));
-	}
+			}
 	
 	public CartPage goToCartPage()
 	{
