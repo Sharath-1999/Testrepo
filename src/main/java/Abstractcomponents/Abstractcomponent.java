1e5021a56d5225eb0d7f15aac5b8a3c8b2edd6f8
Sharath-1999 <sharathchandraracha@gmail.com>
2023-10-28 12:23:26 +0530

    egrtgrth

--- src/main/java/Abstractcomponents/Abstractcomponent.java
+++ src/main/java/Abstractcomponents/Abstractcomponent.java
@@ -43,7 +43,7 @@ public class Abstractcomponent  {
 	{
 		Thread.sleep(1000);
 //		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-//		wait.until(ExpectedConditions.invisibilityOf(ele));
+//		wait.until(ExpectedConditions.invisibilityOf(ele))erter;
 		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
